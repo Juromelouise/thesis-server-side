3d2b9beb1f7c24fba9877703d8c019c72eb93200
Something/ <123807319+Juromelouise@users.noreply.github.com>
2024-09-19 23:49:34 +0800

    2nd commit

--- controller/userController.js
+++ controller/userController.js
@@ -1,2 +1,6 @@
 const User = require("../model/User");
+const sendToken = require("../utils/jwtToken");
 
+exports.registerUser = async (req, res, next) => {};
+
+exports.loginUser = async (req, res, next) => {};
