cc517e7ea1d191026edac9aba6114a5a8fb7555a
unknown <juromefernando@gmail.com>
2025-02-26 16:38:59 +0800

    change violations

--- controller/plateNumber.js
+++ controller/plateNumber.js
@@ -3,9 +3,14 @@ const plateNumber = require("../model/PlateNumber");
 exports.changeViolation = async (req, res) => {
   try {
     const { violations } = req.body;
-    const plate = await plateNumber.findByIdAndUpdate(
-      req.params.id,
-      { violations },
+    // const plate = await plateNumber.findByIdAndUpdate(
+    //   req.params.id,
+    //   { violations },
+    //   { new: true }
+    // );
+    const plate = await plateNumber.findOneAndUpdate(
+      { "violations.report": req.params.id },
+      { $set: { "violations.$.types": violations } },
       { new: true }
     );
     res.status(200).json({ report: plate });
@@ -17,13 +22,13 @@ exports.changeViolation = async (req, res) => {
 
 exports.getAllPlateNumbers = async (req, res) => {
   try {
-    const plateNumbers = await plateNumber.find({count: {$gt: 0}});
+    const plateNumbers = await plateNumber.find({ count: { $gt: 0 } });
     res.status(200).json({ plateNumbers });
   } catch (error) {
     console.error("Error getting all plate numbers:", error);
     res.status(500).json({ message: "Failed to get all plate numbers." });
   }
-}
+};
 
 exports.getPlateNumber = async (req, res) => {
   try {
@@ -33,4 +38,4 @@ exports.getPlateNumber = async (req, res) => {
     console.error("Error getting plate number:", error);
     res.status(500).json({ message: "Failed to get plate number." });
   }
-}
+};
